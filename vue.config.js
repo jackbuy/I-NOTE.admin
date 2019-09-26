c6c5ce914a52f0ecb8d26d11bba2ef34598b06b9
jackbuy <2538362801@qq.com>
2019-09-26 13:38:16 +0800

    update

---
+++ vue.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const resolve = dir => path.join(__dirname, dir)
+// const fs = require('fs')
+
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/production-sub-path/'
+    : '/',
+    outputDir: 'dist',
+    // 配置路径别名
+    chainWebpack: config => {
+        config.resolve.alias
+        .set('@', resolve('src'))
+        .set('_c', resolve('src/components/'))
+        .set('_as', resolve('src/assets/img/'))
+    },
+    // 传递第三方插件选项
+    // pluginOptions: {
+    //     'AMap': 'AMap',
+    //     'QRCode': 'QRCode'
+    // },
+    // 全局注入通用样式
+    // css: {
+    //     loaderOptions: {
+    //         stylus: {
+    //             data: fs.readFileSync('./src/assets/stylus/mixins.styl', 'utf-8')
+    //         }
+    //     }
+    // }
+}
