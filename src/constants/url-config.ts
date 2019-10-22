ca0ac244f4bcef1120b85a7ba4603158c32fc243
jackbuy <2538362801@qq.com>
2019-10-22 16:12:57 +0800

    update

--- src/constants/url-config.ts
+++ src/constants/url-config.ts
@@ -1,6 +1,8 @@
 // 访问图片地址
 // export const imgBaseUrl = 'http://localhost:8081/resouces/';
-export const imgBaseUrl: string = 'http://localhost:10000';
+export const imgBaseUrl: string = 'http://119.23.10.4:10000';
+// export const imgBaseUrl: string = 'http://localhost:10000';
 
 // api地址
-export const apiBaseUrl: string = 'http://localhost:10000';
+export const apiBaseUrl: string = 'http://119.23.10.4:10000';
+// export const apiBaseUrl: string = 'http://localhost:10000';
